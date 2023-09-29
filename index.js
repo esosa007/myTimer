85d401e8d2f9ff6f0baad0ac482127f101a74181
Enrique Sosa <elasosajr@gmail.com>
2023-09-28 19:29:35 -0700

    Starting over

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-const durationInput = document.querySelector('#durationInput');
-const startButton = document.querySelector('#start');
-const pauseButton = document.querySelector('#pause');
-
-const timer = new Timer(durationInput, startButton, pauseButton, {
-    onStart() {
-        console.log('Timer started')
-    },
-    onTick() {
-        console.log('Timer ticked')
-    },
-    onComplete() {
-        console.log('Timer is completed')
-    }
-});
+console.log('Hi')
