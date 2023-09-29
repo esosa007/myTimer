89f2e8f809a384c998e820290e2dced7fd331579
Enrique Sosa <elasosajr@gmail.com>
2023-09-28 19:58:41 -0700

    redo baby

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log('Hi')
+class Timer {
+    constructor(inputDuration, startButton, pauseButton) {
+        this.inputDuration = inputDuration;
+        this.startButton = startButton;
+        this.pauseButton = pauseButton;
+
+        startButton.addEventListener('click', this.startTimer);
+        pauseButton.addEventListener('click', this.pauseTimer);
+    }
+
+    startTimer = () => {
+        console.log('Timer started');
+        this.tick();
+    }
+
+    pauseTimer = () => {
+        console.log('Timer paused');
+        clearInterval(this.interval);
+    }
+
+    tick = () => {
+        this.interval = setInterval(() => {
+            console.log('tick')
+        }, 1000)
+    }
+}
+
+const input = document.querySelector('#inputDuration');
+const start = document.querySelector('#startButton');
+const pause = document.querySelector('#pauseButton');
+
+const timer = new Timer(input, start, pause);
